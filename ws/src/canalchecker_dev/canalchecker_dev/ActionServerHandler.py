b2b1a241e866a7675313d4d1076c5e220c8a9ab9
yaba-source <12479yp@gmx.de>
2025-11-18 20:49:38 +0000

    weiter am Handler

--- ws/src/canalchecker_dev/canalchecker_dev/ActionServerHandler.py
+++ ws/src/canalchecker_dev/canalchecker_dev/ActionServerHandler.py
@@ -1,10 +1,11 @@
 import  rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
+from rclpy.node import Node
 from canalchecker_interface import Align
 from canalchecker_interface import Follow
 from canalchecker_interface import Drive
-from .HandlerLogic import StateMachine
+from logik.HandlerLogic import StateMachine
 
 class ActionServerHandler (Node):
     def __init__(self):
@@ -16,22 +17,47 @@ class ActionServerHandler (Node):
         self.actionserveralign=self.create_client(self,Align, 'align'),
         self.actionserverdrive=self.create_client(self,Drive, 'drive'),
         self.actionserverfollow=self.create_client(self,Follow, 'follow')
+
+        self.timer = self.create_timer(0.1, self.state_machine_loop)
+        
+        self.current_state = None
       
         self.get_logger().info('Handler started')
         
     def run(self):
         """ Starts the State Machine """
-        self.get_logger().info('Starting State Machine execution')
-        
-        try:
+        if self.current_action is None:
             self.state_machine.execute()
-        except Exception as e:
-            self.get_logger().error(f'Error in State Machine: {str(e)}')
+            self.send_goal_for_state()
+    
+    def send_goal_for_state(self):
+        """Sendet Goal basierend auf aktuellem State"""
+        if self.state_machine.state == 10:
+            self.get_logger().info("Sende Align Goal")
+            goal_msg = Align.Goal()
+            # Konfiguriere Align Goal hier
+            self.send_goa l('align', goal_msg)
+        
+        elif self.state_machine.state == 20:
+            self.get_logger().info("Sende Drive Goal")
+            goal_msg = Drive.Goal()
+            # Konfiguriere Drive Goal hier
+            self.send_goal('drive', goal_msg)
+        
+        elif self.state_machine.state == 30:
+            self.get_logger().info("Sende Follow Goal")
+            goal_msg = Follow.Goal()
+            # Konfiguriere Follow Goal hier
+            self.send_goal('follow', goal_msg)
 
 
         
     def send_goal(self, action_type, goal_msg):
-        """Sendet Goal an Action Server"""
+        """Sendet Goal an Action Server
+        Sendet das Goal asynchron an den Action Server
+        Registriert Feedback-Callback während der Ausführung
+        Registriert Callback wenn der Server antwortet (akzeptiert/ablehnt)
+        """
         if action_type == 'align':
             client = self.actionserver_align
         elif action_type == 'drive':
@@ -51,7 +77,8 @@ class ActionServerHandler (Node):
     
     
     def goal_response_callback(self, promise):
-        """Callback if acepted"""
+        """Callback if acepted/ declined
+        """
         goal_handler = promise.result()
         if not goal_handler.accepted:
             self.get_logger().info('Goal rejected :(')
@@ -59,16 +86,37 @@ class ActionServerHandler (Node):
         self.get_logger().info('Goal accepted :)')
         self._get_result_promise = goal_handler.get_result_async()
         self._get_result_promise.add_done_callback(self.get_result_callback)
+
     
     def get_result_callback(self, promise):
-        """"""
-        result = promise.result().result
-        self.get_logger().info('Done: ' + str(result.success))
+        """Answer from Server if done"""
+       if self.current_action == 'align':
+            self.state_machine.set_align_done()
+            self.get_logger().info('Align Server: fertig')
+        
+        elif self.current_action == 'drive':
+            self.state_machine.set_drive_done()
+            self.get_logger().info('Drive Server: fertig')
+        
+        elif self.current_action == 'follow':
+            self.state_machine.set_follow_done()
+            self.get_logger().info('Follow Server: fertig')
+        
+        self.current_action = None
     
     def feedback_callback(self, feedback_msg):
-        """ Rü"""
+        """"""
         feedback = feedback_msg.feedback
-        self.get_logger().info('Received feedback: {0}'.format(feedback))
+       
+
+        if self.current_action == 'align':
+            self.get_logger().info(f'Align Feedback: {feedback}')
+        
+        elif self.current_action == 'drive':
+            self.get_logger().info(f'Drive Feedback: {feedback}')
+        
+        elif self.current_action == 'follow':
+            self.get_logger().info(f'Follow Feedback: {feedback}')
 
     def main(args=None):
         rclpy.init(args=args)
@@ -77,7 +125,6 @@ class ActionServerHandler (Node):
             handler = ActionServerHandler()
             handler.get_logger().info('Drücke Ctrl+C zum Beenden')
 
-            handler.run()
 
             rclpy.spin(handler)
         
--- ws/src/canalchecker_dev/canalchecker_dev/logik/HandlerLogic.py
+++ ws/src/canalchecker_dev/canalchecker_dev/logik/HandlerLogic.py
@@ -1,2 +1,82 @@
-import pytest
-from types import SimpleNamespace
+"""
+Describes the Logic in the which server is called
+First call is align then Drive an if follow id is detected Follow 
+"""
+
+
+class stateMachine:
+    def __init__(self):
+        self.state =0
+        self.error=None
+        self.align_done = False
+        self.drive_done = False
+        self.follow_done = False
+        
+
+    def execute(self):
+        """State Machine for the Workflow"""
+        match self.state:
+            case 10:
+                self.align_state()
+            case 20:
+                self.drive_state()
+            case 30:
+                self.follow_state()
+            case _:
+                if self.logger:
+                    self.logger.error(f"Unknown state: {self.state}")
+                else:
+                    print(f"Unknown state: {self.state}")
+
+        
+    def align_state(self):
+        """State 10: Align Server"""
+        if self.logger:
+            self.logger.info("State 10: Calling Align Server")
+        if self.align_done:
+            if self.logger:
+                self.logger.info("Align complet")
+            self.state = 20
+            self.align_done = False
+    
+    def drive_state(self):
+        if self.drive_done:
+            if self.aruco_detected and self.aruco_id == 69:
+                if self.logger:
+                    self.logger.info("Aruco ID 69 detected, moving to Follow")
+                self.state = 30
+            else:
+                if self.logger:
+                    self.logger.info("Drive completed, returning to Align")
+                self.state = 10
+            
+            self.drive_done = False
+            self.aruco_detected = False
+    def follow_state(self):
+        """State 30: Follow Server"""
+        if self.logger:
+            self.logger.info("State 30: Calling Follow Server")
+        
+        if self.follow_done:
+            if self.logger:
+                self.logger.info("Follow completed, returning to Align")
+            self.state = 10
+            self.follow_done = False
+
+    def set_align_done(self):
+        """Handler will set if align done"""
+        self.align_done = True
+
+    def set_drive_done(self):
+        """Handler will set if drive done"""
+        self.drive_done = True
+
+    def set_follow_done(self):
+        """Handler will set if follow done"""
+        self.follow_done = True
+
+    def set_aruco_detected(self, aruco_id):
+        """Wird vom Handler aufgerufen wenn Aruco erkannt wurde"""
+        self.aruco_detected = True
+        self.aruco_id = aruco_id
+    
