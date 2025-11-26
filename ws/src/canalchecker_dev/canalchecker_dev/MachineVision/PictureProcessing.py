7659a3085195772175efb7f76770b4b9868791ae
yaba-source <12479yp@gmx.de>
2025-11-26 23:07:45 +0100

    Update PictureProcessing.py

--- ws/src/canalchecker_dev/canalchecker_dev/MachineVision/PictureProcessing.py
+++ ws/src/canalchecker_dev/canalchecker_dev/MachineVision/PictureProcessing.py
@@ -1,27 +1,35 @@
-import cv2 as cv
-import numpy as np
-import math
+mport cv2 as cv
 from .ArucoMarkerDetector import ArucoMarkerDetector
-
 
 class PictureProcessing:
     def __init__(self, camera_calib=None):
-        
         self.aruco_detector = ArucoMarkerDetector(camera_calib=camera_calib)
         self.cap = cv.VideoCapture(0)
-
+        
+        # Public Variablen
+        self.has_marker = False
+        self.marker_id = -1
+        self.distance = 0.0
+        self.angle = 0.0
+    
     def process_frame(self):
         ret, frame = self.cap.read()
         if not ret:
-            return None
-
+            self.has_marker = False
+            return False
+        
         if self.aruco_detector.detect_markers(frame):
             self.aruco_detector.estimate_pose()
-            return (True, 
-                    self.aruco_detector.ids,
-                    self.aruco_detector.distances,
-                    self.aruco_detector.angles)
+            self.has_marker = True
+            self.marker_id = int(self.aruco_detector.ids[0][0])
+            self.distance = float(self.aruco_detector.distances[0])
+            self.angle = float(self.aruco_detector.angles[0])
+        else:
+            self.has_marker = False
+            self.marker_id = -1
+            self.distance = 0.0
+            self.angle = 0.0
         
-        return False, 1000, 0, 0
+        return True
 
 
