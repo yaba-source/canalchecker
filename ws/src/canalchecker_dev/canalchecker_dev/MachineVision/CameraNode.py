b6354ec79885589dd9047e22299f33c1b42b1b22
Marcel <modgamerlp@gmail.com>
2025-11-26 12:50:44 +0100

    Publisher für Angle und id erstellt und vorerst mit randomisierten werten befüllt

--- ws/src/canalchecker_dev/canalchecker_dev/MachineVision/CameraNode.py
+++ ws/src/canalchecker_dev/canalchecker_dev/MachineVision/CameraNode.py
@@ -20,17 +20,36 @@ class CameraNode(Node):
             10
         )
 
+        self.publisher_angle = self.create_publisher(
+            UInt32,
+            '/aruco_angle',
+            10
+        )
+
+        self.publisher_id = self.create_publisher(
+            UInt32,
+            '/aruco_id',
+            10
+        )
+
         self.timer = self.create_timer(0.1, self.timer_callback_fnc)
 
     def timer_callback_fnc(self):
         aruco_dist = UInt32()
         aruco_detected = Bool()
+        aruco_angle = UInt32()
+        aruco_id = UInt32()
 
         # 'random' durch funktionscalls ersetzen
         aruco_dist.data = random.randint(0, 10)
         aruco_detected.data = bool(random.randint(0, 1))
+        aruco_angle = random.randint(0, 360)
+        aruco_id = random.randint(0, 999)
+        
         self.publisher_dist.publish(aruco_dist)
         self.publisher_detected.publish(aruco_detected)
+        self.publisher_angle.publish(aruco_angle)
+        self.publisher_id.publish(aruco_id)
 
 
 def main():
