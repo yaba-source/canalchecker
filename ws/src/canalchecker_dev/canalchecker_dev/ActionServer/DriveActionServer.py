0077630a2879cd59304e370fff3c955c5322d3df
Marcel K <modgamerlp@gmail.com>
2025-12-02 21:25:56 +0100

    DriveActionServer logik komplettiert

--- ws/src/canalchecker_dev/canalchecker_dev/ActionServer/DriveActionServer.py
+++ ws/src/canalchecker_dev/canalchecker_dev/ActionServer/DriveActionServer.py
@@ -3,6 +3,8 @@ from rclpy.executors import MultiThreadedExecutor
 from rclpy.action import ActionServer
 from rclpy.node import Node
 from canalchecker_interface.action import Drive
+from canalchecker_interface.msg import ArucoDetection
+from canalchecker_dev.logik.DriveLogic import DriveStateMachine
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.action import ActionServer, GoalResponse
 import time
@@ -19,7 +21,9 @@ class DriveActionServer(Node):
             self,
             Drive,
             'drive',
-            self.execute_callback_fnc #Zuweisung von Phython verwenden  #default verhalten checken
+            execute_callback=self.execute_callback_fnc,
+            goal_callback=self.goal_callback_fnc,
+            callback_group=ReentrantCallbackGroup()
         )
         self.goal_handle = None
 
@@ -36,43 +40,85 @@ class DriveActionServer(Node):
             10
         )
 
-        self.action_server = ActionServer(
-            self,
-            Drive,
-            'drive',
-            execute_callback=self.execute_callback_fnc,
-            goal_callback=self._goal_callback_fnc,
-            handle_accepted_callback=self._handle_accepted_callback_fnc,
-            callback_group=ReentrantCallbackGroup()
+        self.sub_aruco = self.create_subscription(
+            ArucoDetection,
+            '/aruco_detections',
+            self.aruco_callback_fnc,
+            10
         )
 
+        self.aruco_dist = 0
+        self.aruco_angle = 0
+        self.aruco_id = -1
+
    
-    def _goal_callback_fnc(self, goal_request):
+    def goal_callback_fnc(self, goal_request):
+        self.get_logger().info('Goal received')
         return GoalResponse.ACCEPT
 
-    def _handle_accepted_callback_fnc(self, goal_handle):
-        self.goal_handle = goal_handle
-        goal_handle.execute()
-
+    def aruco_callback_fnc(self, msg: ArucoDetection):
+        self.aruco_dist = msg.dist
+        self.aruco_angle = msg.angle
+        self.aruco_id = msg.id
 
     def listener_callback_fnc(self, msg: Odometry):
-       
-        
-        self.get_logger().info(f"Pos X: {msg.pose.pose.position.x:.3f} " f"Pos Y: {msg.pose.pose.position.y:.3f}")
-
+        self.get_logger().info(f"Pos X: {msg.pose.pose.position.x:.3f} " f"Pos Y: {msg.pose.pose.position.y:.3f}")        
 
     def execute_callback_fnc(self, goal_handle): 
         self.get_logger().info('Goal Received! Driving.')
+        cmd = Twist()
         self.goal_handle = goal_handle
         self.goal_finished = False
         self.goal_result = None
-        for i in range(10):
-            if not goal_handle.is_active:
-                break
+        
+        state = DriveStateMachine()
+        while rclpy.ok() and not state.drive_complete:
+            if self.aruco_id == -1:
+                # Wenn kein Aruco Marker erkannt wird, fahre nicht
+                self.get_logger().warning('No Aruco found. Stopping drive.')
+                self.stop_robot()
+            else:
+                self.get_logger().info(f'\nAruco Marker found {self.aruco_id}.\nDistance: {self.aruco_dist}\nAngle: {self.aruco_angle}')
+                state.id = self.aruco_id
+                state.angle = self.aruco_angle
+                state.distance = self.aruco_dist
+            
+            if goal_handle.is_cancel_requested:
+                goal_handle.canceled()
+                self.stop_robot()
+                self.get_logger().info('Goal cancelled. Robot Stopped.')
+                return Drive.Result(reached=False)
+
+            state.execute()
+            if self.aruco_id > -1:
+                cmd.angular.z = state.angular_cmd
+            else:
+                cmd.angular.z = 0
+            cmd.linear.x = state.max_linear_speed
+            self.publisher.publish(cmd)
+
+            feedback = Drive.Feedback()
+            feedback.dist_to_goal = state.distance
+            goal_handle.publish_feedback(feedback)
+        
+        self.stop_robot()
+
+        if state.drive_complete:
+            goal_handle.succeed()
             result = Drive.Result()
-        result.success = True 
-        return result   
+            result.reached = True
+            self.get_logger().info('Drive Complete')
+            return result
+        else:
+            result = Drive.Result()
+            result.reached = False
+            return result 
       
+    def stop_robot(self):
+        cmd = Twist()
+        cmd.linear.x = 0
+        cmd.angular.z = 0
+        self.publisher.publish(cmd)
 
 
 def main():
---
+++ ws/src/canalchecker_dev/canalchecker_dev/logik/DriveLogic.py
@@ -0,0 +1,52 @@
+ANGULAR_Z_STEPSIZE = 0.01
+ANGULAR_Z_MAX = 0.2
+
+class DriveStateMachine:
+    """
+    Interne StateMachine des Driveservers.
+    """
+    def __init__(self):
+        self.state = 1
+        self.error = None
+        self.drive_complete = False
+        self.max_linear_speed = 0.2
+        self.max_angular_speed = 0.2
+        self.id = -1
+        self.distance = 0               # Distance in m
+        self.angle = 0                  # Angle in degrees
+        self.angle_tolerance = 0.05     # Tolerance in degrees
+        self.angular_cmd = 0            # angular speed to be commanded
+
+    def target_actual_comparison(self, target: float, actual: float):
+        """
+        Führt einen Soll / Ist vergleich zwischen zwei werten durch und gibt einen bool wert zurück.
+        
+        :param target: Zielwert welcher erreicht werden soll
+        :type target: float
+        :param actual: Wert welcher eigentlich aktuell ausgelesen wird
+        :type actual: float
+        """
+
+        if target > actual:
+            self.angular_cmd += ANGULAR_Z_STEPSIZE
+            if self.angular_cmd > ANGULAR_Z_MAX:
+                self.angular_cmd = ANGULAR_Z_MAX
+        elif target < actual:
+            self.angular_cmd -= ANGULAR_Z_STEPSIZE
+            if abs(self.angular_cmd) > abs(ANGULAR_Z_MAX):
+                self.angular_cmd = ANGULAR_Z_MAX
+        else:
+            print('[ERROR] Error im Soll / Ist vergleich! Übergebene werte waren:\n Soll: ', target, 'Ist: ', actual)
+            exit()
+    
+    def execute(self):
+        match self.state:
+            case 1:
+                print('[DEBUG] StateMachine Case 1. Driving')
+                if self.distance < 0.15:
+                    self.state = 2
+                if abs(self.angle) > self.angle_tolerance:
+                    self.target_actual_comparison(0, self.angle)
+            case 2:
+                print('[DEBUG] StateMachine Case 2. Finished')
+                self.drive_complete = True
